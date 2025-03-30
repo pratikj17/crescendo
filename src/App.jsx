930a76271aa92706fef979120dd3aa201f06fc2d
pratikj17 <jpratik4002@gmail.com>
2025-03-30 11:36:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import LandingPage from './pages/LandingPage'
 import StudentDashboard from './components/StudentDashboard/StudentDashboard';
 import StudentBatch from './components/StudentDashboard/StudentBatch';
 import Assignments from './components/StudentDashboard/Assignment';
-import SubmittedAssignmentsStudents from './components/StudentDashboard/SubmittedAssignmentStudents';
+// import SubmittedAssignmentsStudents from './components/StudentDashboard/SubmittedAssignmentStudents';
 import AdminLogin from './pages/AdminLogin'
 import TrainerSignup from './pages/TrainerSignup';
 import TrainerLogin from './pages/TrainerLogin';
@@ -32,7 +32,7 @@ function App() {
         <Route path="/studentprofile" element={<StudentProfile/>}/>
         <Route path='/studentdashboard' element={<StudentDashboard />}/>
         <Route path='/studentbatch' element={<StudentBatch />}/>
-        <Route path='/submittedassignmentsstudents' element={<SubmittedAssignmentsStudents />}/>
+        {/* <Route path='/submittedassignmentsstudents' element={<SubmittedAssignmentsStudents />}/> */}
         <Route path='/assignments' element={<Assignments />}/>
         
         {/*Trainer */}
